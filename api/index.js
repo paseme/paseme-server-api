fd47d4706429580a7db2b6a40ea47aa5c933b1e8
Paseme <paseme@yahoo.com>
2023-02-02 09:36:05 -0300

    Conexão com o banco de dados adicionada!

--- api/index.js
+++ api/index.js
@@ -1,5 +1,23 @@
+const { MongoClient } = require("mongodb");
+
 module.exports = async function(req, res) {
 
-    return res.status(200).json({ mensagem: "Foi" })
+    const client = new MongoClient(process.env.MONGODB_URI);
+
+    client.connect()
+
+        .then(function() {
+            
+            return res.status(200).json({ mensagem: "Conectado com o BANCO DE DADOS!" })
+
+        })
+
+        .catch(function(erro) {
+
+            console.log(erro)
+
+            return res.status(500).json({ mensagem: "eRRO ao conectar" })
+
+        });
 
 }
