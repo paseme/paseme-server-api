c542631a2d2feea0bb78c1ba1f097a04284b8651
Paseme <paseme@yahoo.com>
2023-02-02 09:49:46 -0300

    Atualização dos documentos ao MongoDB!

--- api/index.js
+++ api/index.js
@@ -8,16 +8,35 @@ module.exports = async function(req, res) {
 
         .then(function() {
             
-            return res.status(200).json({ mensagem: "Conectado com o BANCO DE DADOS!" })
+            console.log("CONECTADO COM O BANCO DE DADOS!");
+
+            //return res.status(200).json({ mensagem: "CONECTADO COM O BANCO DE DADOS!" });
 
         })
 
         .catch(function(erro) {
 
-            console.log(erro)
+            console.error(erro);
 
-            return res.status(500).json({ mensagem: "eRRO ao conectar" })
+            //return res.status(500).json({ mensagem: "ERRO AO CONECTAR COM O BANCO DE DADOS!" });
 
         });
 
+    client.db("paseme").collection("atendimento").insertOne({ codigo: "oi" })
+
+        .then(function(result) {
+
+            return res.status(200).json({ mensagem: "DOCUMENTO SALVO!" });
+
+        })
+
+        .catch(function(erro) {
+
+            console.error(erro);
+
+            return res.status(500).json({ mensagem: "ERRO AO SALVAR DOCUMENTO!" });
+
+        });
+
+    client.close();
 }
