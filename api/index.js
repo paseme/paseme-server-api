74e362a41d29ec131d802eb9e7917933480376e2
Paseme App <paseme@yahoo.com>
2023-02-17 23:10:31 -0300

    Save pure date in database!

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ export default async function(request, response) {
             
             codigo: nanoid(), 
             
-            tempo: new Date().toString(), 
+            tempo: new Date(), 
             
             trafego: {
 
